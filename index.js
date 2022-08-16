90f14d7e307516842bc8be5377132bb0505c5c74
Ngowwa-Ziro <joseph.ngowwa@student.moringaschool.com>
2022-08-16 09:46:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+ }
+ 
+ function destructivelyPrependCat(){
+    cats.unshift("Bob");
+ }
+
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+ }
+
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+
+ 
+ function appendCat(){
+    appendCat = [...cats,"Broom"];
+    return appendCat;
+ }
+ function prependCat(){
+    prependCat = ["Arnold", ...cats]; 
+    return prependCat;
+ }
+
+ function removeLastCat() {
+    removeLastCat=cats.slice(0,2)
+    return removeLastCat
+}
+function removeFirstCat() {
+    removeFirstCat=cats.slice(1)
+    return removeFirstCat
+}
